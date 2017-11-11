0011a1ebb3ff03124cdf19a65d84bafe01985a42
Shruan <qiushiyuan1994@qq.com>
2017-11-11 14:29:05 +0800

    新增删除字符串两端空格方法

--- comMethods/utils.js
+++ comMethods/utils.js
@@ -72,5 +72,17 @@
          this.seach()
        }
      }).bind(this)
-   }
+   },
+   //删除右边的空格
+   trim (str) {
+     return str.replace(/(^\s*)|(\s*$)/g, '')
+   },
+   //删除左边的空格
+   function ltrim (str) {
+     return str.replace(/(^\s*)/g, '');
+　　},
+    //删除右边的空格
+    function rtrim (str) {
+      return str.replace(/(\s*$)/g, '')
+    }
  }
