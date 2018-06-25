2e7c172253ffbc3814fc0170f73c2beffb116aa3
Shruan <qiushiyuan1994@qq.com>
2018-06-25 16:16:10 +0800

    增加 js 对cookie的 增、删、查操作

--- utils/utils.js
+++ utils/utils.js
@@ -1,43 +1,76 @@
 /*
 * 使用方法
-* import { myDOM } from '@/coMethods/utils.js'
-* myDom.addClass(el, className)
+* import { utils } from '@/coMethods/utils.js'
+* utils.addClass(el, className)
 */
 
-export const myDOM = {
+export const utils = {
+  // cookie 存储、查看、删除
+  cookie: {
+    set: function (name, value) {
+      // 缓存过期时间
+      let Days = 30
+      let exp = new Date()
+      exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000)
+      document.cookie = name + '=' + escape(value) + ';expires=' + exp.toGMTString() + ';path=/'
+    },
+    get: function (name) {
+      let arr = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+      let reg = arr
+      arr = document.cookie.match(reg)
+      if (arr) {
+        return unescape(arr[2])
+      } else {
+        return null
+      }
+    },
+    del: function (name) {
+      let exp = new Date()
+      exp.setTime(exp.getTime() - 1)
+      let cval = this.get(name)
+      if (cval != null) {
+        document.cookie = name + '=' + cval + ';expires=' + exp.toGMTString() + ';path=/'
+      }
+    }
+  },
+
   // 判断是否有该 class
-  hasClass (el, className) {
+  hasClass: function (el, className) {
     let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
     return reg.test(el.className)
   },
 
   // 添加 class
-  addClass (el, className) {
-    if (this.hasClass(el, className)) {
-      return
-    }
+  addClass: function (el, className) {
+    if (this.hasClass(el, className)) return
     let newClass = el.className.split(' ')
     newClass.push(className)
     el.className = newClass.join(' ')
   },
 
-  // 拾取url后面带的参数 如a.html?key=aaaa&name=1232
-  // 调用方式 var Request=new urlCatch(); //实例化
-  // alert(Request.wd);
-  urlCatch: function () {
-    var name , value;
-    var obj = {}
-    var str = location.href; // 取得整个地址栏
-    var num = str.indexOf("?")
-    str = str.substr(num + 1); // 取得所有参数   stringvar.substr(start [, length ]
+  removeClass: function (el, className) {
+    if (this.hasClass(el, className)) {
+      el.className = el.className.replace(new RegExp( '(\\s|^)' + className + '(\\s|$)' ), ' ') // replace方法是替换
+    }
+  },
 
-    var arr = str.split("&"); // 各个参数放到数组里
+  // 拾取url后面带的参数 如a.html?key=aaaa&name=1232
+  // 调用方式 var Request=new urlCatch() //实例化
+  // alert(Request.wd)
+  urlCatch: function () {
+    var name ,value
+    var obj = {}
+    var str = location.href // 取得整个地址栏
+    var num = str.indexOf('?')
+    str = str.substr(num + 1) // 取得所有参数   stringvar.substr(start [, length ]
+
+    var arr = str.split('&') // 各个参数放到数组里
     for (var i = 0; i < arr.length; i++) {
-      num = arr[i].indexOf("=");
+      num = arr[i].indexOf('=')
       if (num > 0) {
-        name = arr[i].substring(0, num);
-        value = arr[i].substr(num + 1);
-        obj[name] = value;
+        name = arr[i].substring(0, num)
+        value = arr[i].substr(num + 1)
+        obj[name] = value
       }
     }
     return obj
@@ -56,23 +89,23 @@ export const myDOM = {
 
  // 加入收藏夹方法
   addFavorite: function () {
-    var url = window.location;
-    var title = document.title;
-    var ua = navigator.userAgent.toLowerCase();
+    var url = window.location
+    var title = document.title
+    var ua = navigator.userAgent.toLowerCase()
     if (ua.indexOf("360se") > -1) {
-      alert("由于360浏览器功能限制，请按 Ctrl+D 手动收藏！");
+      alert("由于360浏览器功能限制，请按 Ctrl+D 手动收藏！")
     } else if (ua.indexOf("msie 8") > -1) {
-      window.external.AddToFavoritesBar(url, title); //IE8
+      window.external.AddToFavoritesBar(url, title) //IE8
     } else if (document.all) {
       try{
-        window.external.addFavorite(url, title);
+        window.external.addFavorite(url, title)
       } catch (e) {
-        alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
+        alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!')
       }
     } else if (window.sidebar) {
-      window.sidebar.addPanel(title, url, "");
+      window.sidebar.addPanel(title, url, "")
     } else {
-      alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
+      alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!')
     }
   },
 
@@ -94,12 +127,12 @@ export const myDOM = {
   },
 
   //删除左边的空格
-  function ltrim (str) {
-    return str.replace(/(^\s*)/g, '');
+  ltrim: function (str) {
+    return str.replace(/(^\s*)/g, '')
 　},
 
   //删除右边的空格
-  function rtrim (str) {
+  rtrim: function (str) {
     return str.replace(/(\s*$)/g, '')
   }
 }
