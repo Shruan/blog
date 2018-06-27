c7fe137a4eb70b8d41cc8f46d706073ecfb6cacf
Shruan <qiushiyuan1994@qq.com>
2018-06-27 10:03:50 +0800

    DOM相关操作

--- utils/dom相关.js
+++ utils/dom相关.js
@@ -6,18 +6,24 @@
 
 export const shy_DOM = {
   // 判断是否有该 class
-  hasClass (el, className) {
+  hasClass: function (el, className) {
     let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
     return reg.test(el.className)
   },
 
   // 添加 class
-  addClass (el, className) {
+  addClass: function (el, className) {
     if (this.hasClass(el, className)) {
       return
     }
     let newClass = el.className.split(' ')
     newClass.push(className)
     el.className = newClass.join(' ')
+  },
+
+  removeClass: function (el, className) {
+    if (this.hasClass(el, className)) {
+      el.className = el.className.replace(new RegExp( '(\\s|^)' + className + '(\\s|$)' ), ' ') // replace方法是替换
+    }
   }
 }
